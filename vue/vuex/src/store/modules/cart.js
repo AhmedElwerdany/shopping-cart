00a933e91b2cbb54f33e1283c711d47088bc0de6
Ahmed Elwerdany <infoalwerdany@gmail.com>
2022-12-20 14:28:57 +0200

    decrement inventory when checkout

--- vue/vuex/src/store/modules/cart.js
+++ vue/vuex/src/store/modules/cart.js
@@ -1,87 +1,108 @@
+import { notify } from '@kyvg/vue3-notification';
+
 import { buyProducts } from '../../api/shop';
 
 const state = () => ({
-    items: [],
-    checkoutStatus: null
-})
+  items: [],
+  checkoutStatus: null,
+});
 
 const getters = {
-    cartProducts: (state, getters, rootState) => {
-        return state.items.map(({id, qty}) => {
-            const product = rootState.products.all.find(product => product.id === id)
-            return {
-                id: product.id,
-                title: product.title,
-                price: product.price,
-                qty
-            }
-        })
-    },
-    cartTotalPrice: (state, getters, rootState) => {
-        return getters.cartProducts.reduce((p,c) => {return (c.price * c.qty) + p}, 0)
-    },
-    // getCheckoutStatus:  (state, getters, rootState) => {
-    //     if (state.checkoutStatus?.status === null) {
-    //         return null
-    //     }else if (state.checkoutStatus.status === true) {
-    //         return 'checkout done 💚'
-    //     }else {
-    //         return 'checkout faild 🔴'
-    //     }
-    // }
-}
+  cartProducts: (state, getters, rootState) => {
+    return state.items.map(({ id, qty }) => {
+      const product = rootState.products.all.find(
+        (product) => product.id === id
+      );
+      return {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        qty,
+      };
+    });
+  },
+  cartTotalPrice: (state, getters, rootState) => {
+    return getters.cartProducts.reduce((p, c) => {
+      return c.price * c.qty + p;
+    }, 0);
+  },
+  // getCheckoutStatus:  (state, getters, rootState) => {
+  //     if (state.checkoutStatus?.status === null) {
+  //         return null
+  //     }else if (state.checkoutStatus.status === true) {
+  //         return 'checkout done 💚'
+  //     }else {
+  //         return 'checkout faild 🔴'
+  //     }
+  // }
+};
 
 const actions = {
-    async checkout ({commit, state}, products) {
-        const savedCartItems = [...state.items]
-        commit('setCheckoutStatus', {status: null})
-        commit('setCartItems', {items : []})
+  async checkout({ commit, state, dispatch}, products) {
+    const savedCartItems = [...state.items];
+    commit("setCheckoutStatus", { status: null });
+    commit("setCartItems", { items: [] });
 
-        try {
-            await buyProducts(products)
-            commit('setCheckoutStatus', {status: true})
-            // commit('products/decrementProductInventory', {products}, {root: true})
-        }catch (err) {
-            commit('setCheckoutStatus', {status: false})
-            commit('setCartItems', {items : savedCartItems})
-        }
-        
-    },
-    addProductToCart ({state, commit}, product) {
-        console.log({product})
-        commit('setCheckoutStatus', {status: null})
-        if (product.inventory > 0) {
-            const cartItem = state.items.find(items => items.id === product.id)
-            if (cartItem) {
-                commit('incrementItemQuantity', cartItem)
-            }else {
-                commit('pushProductToCart', product)
-            }
-        }
+    try {
+      await buyProducts(products);
+      commit("setCheckoutStatus", { status: true });
+      dispatch('products/decrementProductInventoryAction', {products}, {root: true})
+      notify({
+        group: "main",
+        title: "checkout",
+        text: "your order was confirmed",
+      });
+      // commit('products/decrementProductInventory', {products}, {root: true})
+    } catch (err) {
+        console.log({err})
+      notify({
+        group: "main",
+        title: "checkout",
+        text: "something happened, please try again later",
+        type: "error",
+      });
+      commit("setCheckoutStatus", { status: false });
+      commit("setCartItems", { items: savedCartItems });
     }
-}
+  },
+  addProductToCart({ state, commit }, product) {
+    commit("setCheckoutStatus", { status: null });
+    notify({
+      group: "main",
+      title: product.title,
+      text: "product added successfully",
+    });
+    if (product.inventory > 0) {
+      const cartItem = state.items.find((items) => items.id === product.id);
+      if (cartItem) {
+        commit("incrementItemQuantity", cartItem);
+      } else {
+        commit("pushProductToCart", product);
+      }
+    }
+  },
+};
 
 const mutations = {
-    setCheckoutStatus (state, payload) {
-        state.checkoutStatus = payload.status
-    },
-    setCartItems (state, payload) {
-        state.items = payload.items
-    },
-    incrementItemQuantity (state, payload) {
-        const cartItem = state.items.find(item => item.id === payload.id)
-        cartItem.qty++
-    },
-    pushProductToCart (state, payload) {
-        state.items.push({id: payload.id, qty: 1})
-    }
-}
-
+  setCheckoutStatus(state, payload) {
+    state.checkoutStatus = payload.status;
+  },
+  setCartItems(state, payload) {
+    state.items = payload.items;
+  },
+  incrementItemQuantity(state, payload) {
+    const cartItem = state.items.find((item) => item.id === payload.id);
+    cartItem.qty++;
+  },
+  pushProductToCart(state, payload) {
+    state.items.push({ id: payload.id, qty: 1 });
+  },
+};
 
 export default {
-    namespaced: true,
-    state,
-    getters,
-    actions,
-    mutations,
-}
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
--- vue/vuex/src/store/modules/product.js
+++ vue/vuex/src/store/modules/product.js
@@ -11,30 +11,30 @@ const actions = {
          const data = await getProducts()
          commit('setAllProducts', {items: data})
     },
+    decrementProductInventoryAction: ({state, commit}, payload) => {
+        const {products} = payload
+        commit('decrementProductInventory', {products})
+    }
 }
 
 const mutations = {
     setAllProducts (state, payload) {
         state.all = payload.items
     },
-    // decrementProductInventory (state, payload) {
-    //     console.log('HIIII')
-    //     const cartItems = payload.products
-    //     console.log(cartItems)
-    //     state.all = state.all.map(product => {
-    //         const {qty} = cartItems.find(cartItem => cartItem.id === product.id )
-    //         console.log('qty', qty)
-    //         console.log( {
-    //             ...product,
-    //             inventory: product.inventory - qty
-    //         })
-    //         return ({
-    //             ...product,
-    //             inventory: product.inventory - qty
-    //         })
-    //     })
-    //     console.log('state.all', state.all)
-    // }
+    decrementProductInventory (state, payload) {
+        const cartItems = payload.products
+        state.all = state.all.map(product => {
+            const cartItem = cartItems.find(cartItem => cartItem.id === product.id)
+            if(cartItem) {
+                return ({
+                    ...product,
+                    inventory: product.inventory - cartItem.qty
+                })
+            }else {
+                return product
+            }
+        })
+    }
 }
 
 
